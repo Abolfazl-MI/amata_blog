6f1db1b7c0580d2956dc68d392e8595cb0d2cc10
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-08-25 12:07:49 -0400

    core files had add

---
+++ lib/core/core.dart
@@ -0,0 +1,7 @@
+
+
+// barell file
+
+export './failure_modle.dart';
+export './opereation_enum.dart';
+export './raw_data_model.dart';
---
+++ lib/core/raw_data_model.dart
@@ -0,0 +1,7 @@
+import 'package:blog_app/core/opereation_enum.dart';
+
+class RawData {
+  OpereationResualt? opereationResualt;
+  dynamic data;
+  RawData({this.data, this.opereationResualt});
+}
