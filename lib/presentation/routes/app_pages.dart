6cfd28063aeb249c1251c44805e92abd3018292f
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-08-25 06:49:48 -0400

    Revert "splash screen designed"
    This reverts commit 5c70f7ec358dbb98384d46a548868bc94b78cb50.
    
    splash screen bug fixed

--- lib/presentation/routes/app_pages.dart
+++ lib/presentation/routes/app_pages.dart
@@ -1,6 +1,5 @@
 import 'package:beamer/beamer.dart';
 import 'package:blog_app/presentation/routes/app_route_names.dart';
-import 'package:blog_app/presentation/screens/splash/splash_screen.dart';
 
 import '../screens/screens.dart';
 
@@ -8,11 +7,10 @@ class AppPages {
   static final routerDelagate = BeamerDelegate(
     locationBuilder: RoutesLocationBuilder(
       routes: {
-       AppRouteNames.splashScreen:(contex,state,data)=>SplashScreen(),
         AppRouteNames.homeScreen: (context, state, data) => HomeScreen(),
         AppRouteNames.loginScree: (context, state, data) => LoginScreen(),
         AppRouteNames.signupScreen: (context, state, data) => SignUpScreen(),
       },
-    ),  
+    ),
   );
 }
--- lib/presentation/routes/app_route_names.dart
+++ lib/presentation/routes/app_route_names.dart
@@ -1,6 +1,5 @@
 abstract class AppRouteNames {
-  static const String splashScreen = '/';
-  static const String homeScreen = '/home_screen';
+  static const String homeScreen = '/';
   static const String loginScree = '/login_screen';
   static const String signupScreen = '/signup_screen';
 }
