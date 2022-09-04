77b8b8b5b17fa14fd2dcf031d8c40c1294b04600
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-09-04 04:36:03 -0400

    updated home repository

--- lib/data/repositories/home_repository.dart
+++ lib/data/repositories/home_repository.dart
@@ -1,11 +1,15 @@
 import 'package:blog_app/core/core.dart';
 import 'package:blog_app/data/models/articles/article_modle.dart';
+import 'package:blog_app/data/models/user/user_modle.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// handles hoem functions such fetching articles and fetching user profile and ...
+
 class HomeRepository {
-  final CollectionReference _articleRef =
+  final CollectionReference _articleRef = FirebaseFirestore.instance
+      .collection(DatabaseConstants.articlesCollection);
+  final CollectionReference _userRef =
       FirebaseFirestore.instance.collection(DatabaseConstants.usersCollection);
-  // handles hoem functions such fetching articles and fetching user profile and ...
 // fetch the all articles exists
   Future<RawData> fetchAllPost() async {
     try {
@@ -17,6 +21,23 @@ class HomeRepository {
             rawData.map((e) => Article.fromJson(e)).toList();
         return RawData(
             operationResult: OperationResult.success, data: articles);
+      } else {
+        return RawData(
+            operationResult: OperationResult.fail, data: 'sth went wrong');
+      }
+    } catch (e) {
+      return RawData(operationResult: OperationResult.fail, data: e.toString());
+    }
+  }
+
+  Future<RawData> fetchSavedArticle(String userUid) async {
+    try {
+      DocumentSnapshot result = await _userRef.doc(userUid).get();
+      if (result.exists) {
+        Map<String, dynamic> rawData = result.data() as Map<String, dynamic>;
+        List<Article> savedArticle = rawData['savedArticles'];
+        return RawData(
+            operationResult: OperationResult.success, data: savedArticle);
       } else {
         return RawData(
             operationResult: OperationResult.fail, data: 'sth went wrong');
