97e93d4eda2190fd9343e287be67578833155285
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-09-06 04:48:07 -0400

    complited the home bloc

--- lib/Blocs/home_bloc/home_bloc.dart
+++ lib/Blocs/home_bloc/home_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:blog_app/core/core.dart';
 import 'package:blog_app/data/models/articles/article_modle.dart';
 import 'package:blog_app/data/repositories/home_repository.dart';
+import 'package:blog_app/data/repositories/user_repository.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -12,8 +13,10 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final HomeRepository _homeRepository;
-  HomeBloc({HomeRepository? homeRepository})
+  final UserRepository _userRepository;
+  HomeBloc({HomeRepository? homeRepository, UserRepository? userRepository})
       : _homeRepository = homeRepository ?? HomeRepository(),
+        _userRepository = userRepository ?? UserRepository(),
         super(HomeLoadingState()) {
     on<LoadAllArticleEvent>(_loadAllArticle);
     on<LoadSavedArticlesEvent>(_loadSavedArticles);
@@ -21,12 +24,14 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     on<RemoveSavedArticleEvent>(_removeArticle);
     on<GetProfileInfoEvent>(_getProfileInfo);
   }
+  List<Article> _allArticles = [];
 
   FutureOr<void> _loadAllArticle(
       LoadAllArticleEvent event, Emitter<HomeState> emit) async {
     emit(HomeLoadingState());
     RawData rawData = await _homeRepository.fetchAllPost();
     if (rawData.operationResult == OperationResult.success) {
+      _allArticles = rawData.data;
       emit(HomeLoadedState(rawData.data));
     }
     if (rawData.operationResult == OperationResult.fail) {
@@ -48,11 +53,37 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   }
 
   FutureOr<void> _saveArticle(
-      SaveArticleEvent event, Emitter<HomeState> emit) {}
+      SaveArticleEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    RawData rawData = await _userRepository.saveArticleToReadingList(
+        user: event.currentUser, article: event.selectedArticle);
+    if (rawData.operationResult == OperationResult.success) {
+      emit(HomeLoadedState(_allArticles));
+    }
+    if (rawData.operationResult == OperationResult.fail) {
+      emit(HomeErrorState(rawData.data));
+    }
+  }
 
   FutureOr<void> _removeArticle(
-      RemoveSavedArticleEvent event, Emitter<HomeState> emit) {}
+      RemoveSavedArticleEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    RawData rawData = await _userRepository.deleteArticleReadingList(
+        user: event.currentUser, article: event.selectedArticle);
+    if (rawData.operationResult == OperationResult.success) {
+      emit(HomeLoadedState(_allArticles));
+    }
+    if (rawData.operationResult == OperationResult.fail) {
+      emit(HomeErrorState(rawData.data));
+    }
+  }
 
   FutureOr<void> _getProfileInfo(
-      GetProfileInfoEvent event, Emitter<HomeState> emit) {}
+      GetProfileInfoEvent event, Emitter<HomeState> emit) async {
+    RawData rawData = await _userRepository.getProfileInfo();
+    if (rawData.operationResult == OperationResult.success) {}
+    if (rawData.operationResult == OperationResult.fail) {
+      emit(HomeErrorState(rawData.data));
+    }
+  }
 }
